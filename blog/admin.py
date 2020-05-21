627ab8aabbfb75bcc40a0df68b0c56fe4135a693
chanyoung <young9484825@gmail.com>
2020-05-21 15:05:03 +0900

    add tag

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,13 @@ from blog.models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):  # Post 클래스가 Admin 사이트에서 어떤 모습으로 보여줄지 정의하는 클래스
-    list_display = ('id', 'title', 'modify_dt')
+    list_display = ('id', 'title', 'modify_dt', 'tag_list')
     list_filter = ('modify_dt',)
     search_fields = ('title', 'content')
     prepopulated_fields = {'slug': ('title',)}
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('tags')
+
+    def tag_list(self, obj):
+        return ', '.join(o.name for o in obj.tags.all())
