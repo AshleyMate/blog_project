ce5016eb2d1e3f6389b11c2a34ee585654c8bbf3
chanyoung <young9484825@gmail.com>
2020-05-27 01:10:35 +0900

    add photo edit templates

--- photo/views.py
+++ photo/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView, DeleteView
-from mysite.views import OwnerOnlyMixin
+from myblog.views import OwnerOnlyMixin
 from photo.forms import PhotoInlineFormSet
 
 
